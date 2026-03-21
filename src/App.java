013dfd57f7c9f6e8902c6c92174e179287c3b104
glennkok1501 <kok.glenn@yahoo.com.sg>
2026-03-21 16:39:56 +0800

    updated battle controller

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import controller.GameController;
 public class App {
     public static void main(String[] args) throws InterruptedException {
         GameController gameController = new GameController();
-        gameController.run(1, 1);
+        gameController.run(2, 2);
 
         
     }
